b89250bd8b44cd5973c86bdd393eacf180ddbf56
Oliver Delevingne <oliver@delevingne.co.uk>
2015-02-05 14:57:58 +0000

    Game has a starting score of zero

--- spec/gameSpec.js
+++ spec/gameSpec.js
@@ -1,3 +1,5 @@
+var Game = require('../src/game')
+
 describe('Game', function(){
   it('has a zero score as default', function(){
     var game = new Game();
---
+++ src/game.js
@@ -0,0 +1,7 @@
+function Game() {
+
+  this.score = 0;
+
+}
+
+module.exports = Game;
