8de8e72fb8eda592f5a0e1339c4835d155ca1fad
Oliver Delevingne <oliver@delevingne.co.uk>
2015-02-05 15:15:56 +0000

    playFrame function test written

--- spec/gameSpec.js
+++ spec/gameSpec.js
@@ -1,8 +1,18 @@
 var Game = require('../src/game')
 
 describe('Game', function(){
+  var game;
+
+  beforeEach(function(){
+    game = new Game();
+  })
+
   it('has a zero score as default', function(){
-    var game = new Game();
     expect(game.score).toEqual(0);
+  });
+
+  it('should be able to play a frame', function(){
+    game.playFrame(2,5);
+    expect(game.score).toEqual(7);
   });
 });
